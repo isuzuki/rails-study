9af81db54b5bbb87c52d89c5ba9f7bfc0803cce9
isuzuki <ikkuntech@gmail.com>
2018-08-28 02:27:53 +0900

    added my 404 routing

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,5 @@
 class ApplicationController < ActionController::Base
+  def render_404
+    render template: 'errors/404', status: 404, layout: 'application', content_type: 'text/html'
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,13 @@
 Rails.application.routes.draw do
   resources :items, only: [:index]
+
+  # GETの404の記述例
+  # get '*not_found', controller: 'application', action: 'render_404'
+  # get '*not_found' => 'application#render_404'
+
+  # matchの404の記述例
+  match '*not_found' => 'application#render_404', via: :all
+  # match '*not_found', :to => 'application#render_404', via: :all
+  # この書き方はsyntaxエラーになる
+  # match '*not_found', controller: 'application', action: 'render_404', via: all:
 end
